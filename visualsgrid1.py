ebfaf011f50e4a952e2ca5fea33ff8349325a596
Alan Jones <jones.alan@hotmail.co.uk>
2022-10-06 14:22:16 +0200

    update

---
+++ visualsgrid1.py
@@ -0,0 +1,36 @@
+import streamlit as st
+
+st.set_page_config(layout='wide')
+
+# make any grid with a function
+
+def make_grid(cols,rows):
+    grid = [0 for i in range(cols)]
+    for i in range(cols):
+        with st.container():
+            grid[i] = st.columns(rows)
+    return grid
+        
+
+mygrid = make_grid(2,6)
+
+mygrid[0][0].subheader('Simple Text:')
+mygrid[0][1].image('images/plttext.png',use_column_width=True)
+mygrid[0][2].subheader('Table:')
+mygrid[0][3].image('images/dataframe.png', width = 300)
+mygrid[0][4].subheader('Heatmap:')
+mygrid[0][5].image('images/heatmapsns.png')
+mygrid[1][0].subheader('Scatter plot:')
+mygrid[1][1].image('images/scatter.png')
+mygrid[1][2].subheader('Line plot:')
+mygrid[1][3].image('images/lineplotshaded.png')
+mygrid[1][4].subheader('Slopegraph:')
+mygrid[1][5].image('images/slope.png')
+
+mygrid2 = make_grid(2,3)
+mygrid2[0][0].image('images/plttext.png',use_column_width=True)
+mygrid2[0][1].image('images/dataframe.png', width = 500)
+mygrid2[0][2].image('images/heatmapsns.png')
+mygrid2[1][0].image('images/scatter.png')
+mygrid2[1][1].image('images/lineplotshaded.png')
+mygrid2[1][2].image('images/slope.png')
